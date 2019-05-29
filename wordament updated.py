6f99d54f039fc9b093d8f20cc79c425b698e608b
Pushkar-Singh-14 <46190572+Pushkar-Singh-14@users.noreply.github.com>
2019-05-29 20:56:55 +0530

    Create wordament updated.py

---
+++ wordament updated.py
@@ -0,0 +1,98 @@
+import random
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+fig = plt.figure(figsize=(13,8), dpi=100)
+ax1 = fig.add_subplot(1,1,1)
+
+Data={}
+i=1
+lines=[]
+count=0
+path="/Users/pushkarsingh/Desktop/word power made easy copy.txt"
+path_report="/Users/pushkarsingh/Desktop/Tests/Reports/worament test1.txt"
+path_rev="/Users/pushkarsingh/Desktop/revise_words.txt"
+path_rev_test="/Users/pushkarsingh/Desktop/revise_words_training.txt"
+f_report=open(path,"r")
+f_rev=open(path_rev,"a+")
+f_rev_test=open(path_rev_test,"a+")
+for name in f_report.readlines():
+    lines.append(name)
+    
+f_report.close()
+
+for line in lines:
+    try:
+        a=str()
+        words= line.split()
+        
+        
+        page_num=words[0]
+        boo= words[1]
+        for word in words[2:]:
+            a=a+word+" "
+        Data.update({i:[page_num,boo,a]})
+        i+=1
+        
+    except:
+        pass
+ 
+page_number=[]
+Word=[]
+Def=[]
+j=1
+for key,value in Data.items():
+    
+    page_number.append(value[0])
+    Word.append(value[1])
+    Def.append(value[2])
+
+num_list=[]
+last_150=len(Word)-150
+last_100=len(Word)-100
+last_50=len(Word)-50
+for i in range(last_50,len(Word)):
+    num_list.append(i)
+    c=len(num_list)
+for i in range(c):
+    try:
+        x=random.randint(0,len(num_list))
+        y=num_list[x]
+        num_list.remove(y)
+
+        x_input = input()
+        if (x_input)=="":
+            print("{}/{}".format(str(j),str(c)))
+            print(Word[y])
+            j+=1
+
+        z_input = input()
+        if (z_input)=="n":
+            print(Def[y],page_number[y])
+            f_rev.write(("{} : {} {} \n \n".format(Word[y],Def[y],page_number[y])))
+            f_rev_test.write(("{} {} {} \n".format(page_number[y],Word[y],Def[y])))
+        else:
+            print(Def[y],page_number[y])
+            
+        
+        
+            
+    except:
+        pass
+f_rev.close()
+f_report=open(path_report,"a+")
+f_report.write(("{}\n".format(j)))
+f_report.close()
+Data1=[]    
+f_report=open(path_report,"r")    
+for name in f_report.readlines():
+    Data1.append(name)
+f_report.close()
+xar=[]
+yar=[]
+for data in Data1:
+    xar.append(float(data))
+    count+=1
+    yar.append(count)
+plt.plot(yar,xar)
+plt.show()
